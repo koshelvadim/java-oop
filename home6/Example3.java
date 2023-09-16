9be173059cccec8b5dc086f7e740bb1ac7e7b9b6
koshelvadim <koshelvadim@mail.ru>
2023-09-16 18:56:26 +0300

    Выполнил 6 ДЗ

---
+++ home6/Example3.java
@@ -0,0 +1,24 @@
+package home6;
+
+public class Example3 {
+    /**
+     * Добавляется интерфейс и теперь класс Car зависит от этого интерфейса,
+     * а класс PetrolEngine имлементирует его и реализует необходимые методы.
+     */
+    public interface IEngine {
+        public void start();
+    }
+    public class Car {
+        private IEngine engine;
+        public Car(IEngine engine) {
+            this.engine = engine;
+        }
+        public void start() {
+            this.engine.start();
+        }
+    }
+    public class PetrolEngine implements IEngine{
+        public void start() {
+        }
+    }
+}
