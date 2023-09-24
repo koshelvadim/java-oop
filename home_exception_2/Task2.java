d8646aec3404b236f693f4db97a0e5971d766393
koshelvadim <koshelvadim@mail.ru>
2023-09-24 17:07:23 +0300

    выполнил 2 ДЗ

---
+++ home_exception_2/Task2.java
@@ -0,0 +1,16 @@
+package home_exception_2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        try {
+            int[] intArray = {1,2,3,4,5,6,7,8,9};
+            int d = 0;
+            double catchedRes1 = (double) intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+
+}
